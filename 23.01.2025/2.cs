e5ac18c91d64036a2726c56d103a84b3ef71f0f8
Stepan Volkov <102189717+stsvift@users.noreply.github.com>
2025-01-23 09:12:05 +0300

    Create 2.cs

---
+++ 23.01.2025/2.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            for (int j = 1; j <= 10; j++)
+                Console.Write((i * j).ToString().PadLeft(4));
+            Console.WriteLine();
+        }
+    }
+}
